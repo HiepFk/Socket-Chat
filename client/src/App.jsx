73e426b98fd72be9e54817a26b891c0372206d98
Nguyễn Huy Hiệp <hiepnh.fk@gmail.com>
2025-02-14 15:55:33 +0700

    1

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,35 +3,39 @@ import io from "socket.io-client";
 
 import SelectRoom from "./components/SelectRoom";
 import ChatRoom from "./components/ChatRoom";
+import ChatBox from "./ChatBox";
 
-const socket = io.connect("http://localhost:8000");
+// const socket = io.connect("http://localhost:8000");
+const socket = io.connect("http://172.16.13.165:8000");
 
 function App() {
   const [showChat, setShowChat] = useState(false);
   const [userName, setUserName] = useState("");
   const [room, setRoom] = useState("");
   return (
-    <div className="App">
-      <header>
-        <h1>⚛️🔥💬</h1>
-        <h1>{showChat ? "Chat Room" : "Select Room"}</h1>
-      </header>
+    // <div className="App">
+    //   <header>
+    //     {/* <h1>⚛️🔥💬</h1> */}
+    //     <h1>Chat Room</h1>
+    //     <h1 className="title">{showChat ? "Chat Room" : "Select Room"}</h1>
+    //   </header>
 
-      <section>
-        {showChat ? (
-          <ChatRoom socket={socket} userName={userName} room={room} />
-        ) : (
-          <SelectRoom
-            socket={socket}
-            userName={userName}
-            setUserName={setUserName}
-            room={room}
-            setRoom={setRoom}
-            setShowChat={setShowChat}
-          />
-        )}
-      </section>
-    </div>
+    //   <section>
+    //     {showChat ? (
+    //       <ChatRoom socket={socket} userName={userName} room={room} />
+    //     ) : (
+    //       <SelectRoom
+    //         socket={socket}
+    //         userName={userName}
+    //         setUserName={setUserName}
+    //         room={room}
+    //         setRoom={setRoom}
+    //         setShowChat={setShowChat}
+    //       />
+    //     )}
+    //   </section>
+    // </div>
+    <ChatBox />
   );
 }
 
---
+++ client/src/ChatBox.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function ChatBox() {
+  return (
+    <iframe
+      src="/build/index.html"
+      width="100%"
+      height="100%"
+      style={{ border: "none" }}
+    ></iframe>
+  );
+}
+
+export default ChatBox;
